11a656047dcb138d639314b566f4f61dcf50b5e7
Butch Griffin <butchg@comcast.net>
2025-05-17 14:27:45 -0700

    finished changing to the XeroTabbedWidget for tabs on the scouting pages (editor and scouter).

---
+++ renderer/src/views/forms/scoutpage.ts
@@ -0,0 +1,31 @@
+import { XeroWidget } from "../../widgets/xerowidget.js";
+import { FormControl } from "./controls/formctrl.js";
+import { XeroEditFormView } from "./editformview.js";
+
+export class XeroFormScoutSectionPage extends XeroWidget {
+    private controls_ : FormControl[] = [] ;
+    private image_ : HTMLImageElement ;
+
+    public constructor(data: string) {
+        super('div', 'xero-form-section-page') ;
+
+        this.image_ = document.createElement('img') ;
+        this.image_.className = 'xero-form-section-image' ;
+        this.image_.src = `data:image/jpg;base64,${data}` ;
+    }
+
+    public get controls() : FormControl[] {
+        return this.controls_ ;
+    }
+
+    public addControl(control: FormControl) : void {
+        this.controls_.push(control) ;
+
+        let top = this.elem.getBoundingClientRect().top ;
+        control.createForScouting(this.elem, 0, top) ;
+    }
+
+    public setImage(data: string) : void {
+        this.image_.src = `data:image/jpg;base64,${data}` ;
+    }
+}
