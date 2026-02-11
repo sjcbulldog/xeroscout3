3060f88d85b941561b2d559030eae5e6048759b8
Luke Puppo <luke@satvantage.com>
2026-02-10 16:14:54 -0800

    Fixed QM sorting, adding Press/Hold vs Start Stop toggle for stopwatch

--- renderer/src/views/forms/dialogs/editstopwatchdialog.ts
+++ renderer/src/views/forms/dialogs/editstopwatchdialog.ts
@@ -3,15 +3,30 @@ import { FormControl } from "../controls/formctrl.js";
 import { IPCStopwatchItem } from "../../../shared/ipc.js";
 
 export class EditStopwatchDialog extends EditFormControlDialog {
+    private hold_mode_?: HTMLInputElement ;
+
     constructor(formctrl: FormControl) {
         super('Edit Stopwatch', formctrl) ;
     }
 
     protected async populateDialog(pdiv: HTMLElement): Promise<void> {
+        let item = this.formctrl_.item as IPCStopwatchItem ;
         let div = document.createElement('div') ;
         div.className = 'xero-popup-form-edit-dialog-rowdiv' ;
 
         this.populateTag(div) ;
+
+        this.hold_mode_ = document.createElement("input") ;
+        this.hold_mode_.type = "checkbox" ;
+        this.hold_mode_.checked = item.holdMode ?? true ;
+        this.hold_mode_.className = "xero-popup-form-edit-dialog-checkbox" ;
+
+        let mode_label = document.createElement("label") ;
+        mode_label.className = "xero-popup-form-edit-dialog-label" ;
+        mode_label.innerText = "Hold (unchecked = Toggle)" ;
+        mode_label.appendChild(this.hold_mode_) ;
+        div.appendChild(mode_label) ;
+
         this.populateColors(div) ;
         await this.populateFontSelector(div) ;
 
@@ -29,6 +44,7 @@ export class EditStopwatchDialog extends EditFormControlDialog {
         item.fontWeight = this.font_weight_!.value ;
         item.fontStyle = this.font_style_!.value ;
         item.transparent = this.transparent_!.checked ;
+        item.holdMode = this.hold_mode_ ? this.hold_mode_.checked : true ;
     }
 
     setFocus() {
@@ -42,4 +58,3 @@ export class EditStopwatchDialog extends EditFormControlDialog {
         setTimeout(this.setFocus.bind(this), 100) ;
     }
 }
-
